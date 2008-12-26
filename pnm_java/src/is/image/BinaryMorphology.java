330b0ec98ddfe14449a4588b2442914a435ed7c4
Jay Anderson <horndude77@gmail.com>
2008-12-26 09:13:44 -0700

    Added disk structuring element and method for ORing images.

--- pnm_java/src/is/image/BinaryMorphology.java
+++ pnm_java/src/is/image/BinaryMorphology.java
@@ -19,6 +19,33 @@ public class BinaryMorphology
         return output;
     }
 
+    public static BilevelImage disk(int rows, int cols)
+    {
+        BilevelImage output = new BilevelImage(rows, cols);
+
+        double a = (cols-1)/2.0;
+        double b = (rows-1)/2.0;
+        double centerRow = (rows-1)/2.0;
+        double centerCol = (cols-1)/2.0;
+
+        for(int row=0; row<rows; ++row)
+        {
+            for(int col=0; col<cols; ++col)
+            {
+                double y = row-centerRow;
+                double x = col-centerCol;
+                double val = (x*x)/(a*a) + (y*y)/(b*b);
+                //System.out.println("("+col+", "+row+") -> ("+x+", "+y+")"+" - "+val);
+                if(val <= 1.0)
+                {
+                    output.set(row, col, BilevelImage.BLACK);
+                }
+            }
+        }
+
+        return output;
+    }
+
     public static BilevelImage dilation(BilevelImage image, BilevelImage st, int rowOrigin, int colOrigin)
     {
         int rows = image.getRows();
@@ -110,6 +137,30 @@ public class BinaryMorphology
         BilevelImage output = image;
         output = erosion(output, st, rowOrigin, colOrigin);
         output = dilation(output, st, rowOrigin, colOrigin);
+        return output;
+    }
+
+    public static BilevelImage or(BilevelImage image1, BilevelImage image2)
+    {
+        int rows = image1.getRows();
+        int cols = image1.getCols();
+        int rows2 = image2.getRows();
+        int cols2 = image2.getCols();
+        if(rows != rows2 || cols != cols2)
+        {
+            throw new RuntimeException("Images must have same dimensions.");
+        }
+
+        BilevelImage output = new BilevelImage(rows, cols);
+
+        for(int row=0; row<rows; ++row)
+        {
+            for(int col=0; col<cols; ++col)
+            {
+                output.set(row, col, (byte) (image1.get(row, col) | image2.get(row, col)));
+            }
+        }
+
         return output;
     }
 }
